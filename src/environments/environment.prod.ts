0a44d54978de059e454e4d824c54f050aa5b4534
Menik <menikdisney@gmail.com>
2019-04-29 12:34:38 +0600

    Add AngularFireModule, AngularFireDatabaseModule, AngularFireAuth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBb4o3eeMCUqqDaefwErrXH6PEcdeL8__E',
+    authDomain: 'organicshop-d6395.firebaseapp.com',
+    databaseURL: 'https://organicshop-d6395.firebaseio.com',
+    projectId: 'organicshop-d6395',
+    storageBucket: 'organicshop-d6395.appspot.com',
+    messagingSenderId: '1002170378242'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBb4o3eeMCUqqDaefwErrXH6PEcdeL8__E',
+    authDomain: 'organicshop-d6395.firebaseapp.com',
+    databaseURL: 'https://organicshop-d6395.firebaseio.com',
+    projectId: 'organicshop-d6395',
+    storageBucket: 'organicshop-d6395.appspot.com',
+    messagingSenderId: '1002170378242'
+  }
+
 };
 
 /*
