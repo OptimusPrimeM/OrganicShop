59254c61acade47f7327e3a2bf1b4e6a6a63aa9f
Menik <menikdisney@gmail.com>
2019-05-06 10:52:41 +0600

    Extracting service to add testability and minimize the lack of seperation

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,27 +1,18 @@
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
 
-  user$: Observable<firebase.User>;
-
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
-  }
-
-  ngOnInit() {
-  }
+  constructor(public auth: AuthService) { }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 
 }
