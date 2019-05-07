03f127e60171f02004bd378e7d28d4cfb65bb2c7
Menik <menikdisney@gmail.com>
2019-05-07 11:38:20 +0600

    Redirect user accordingly aftre different login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OrganicShop';
+
+  constructor(private auth: AuthService, private router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+
+  }
 }
